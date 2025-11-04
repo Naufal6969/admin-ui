bf73ed6aed4eb254ee12b43f930c58cfe9cab888
Naufal6969 <naufalking66@gmail.com>
2025-11-04 07:27:38 +0700

    Understanding JSX in ReactJS and ES6 (2)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+// Lokasi: src/main.jsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
 import "./index.css";
-import Exercise from "../components/Exercise.tsx";
+import Exercise from "./Exercise.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
